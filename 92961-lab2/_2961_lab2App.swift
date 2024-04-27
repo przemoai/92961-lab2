7cfcc0a3d459c8f3a70149ad31c8846c0ef8f33e
x <x@x.com>
2024-04-27 08:04:42 +0200

    Initial Commit

---
+++ 92961-lab2/_2961_lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  _2961_lab2App.swift
+//  92961-lab2
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct _2961_lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
